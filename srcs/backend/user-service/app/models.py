306148e5b27c887a57204dd2cc1bfe1ca33c3f1d
ankammer <ankammer@f1br6s19.paris.42.school>
2026-03-02 17:53:42 +0100

    ADD:models.py microservices

---
+++ srcs/backend/user-service/app/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+# Table des users (login, pswd, mail etc)
+class user(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(50), unique=True, index=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+
+    is_active = Column(Boolean, nullable=False, server_default="true")
+    is_superuser = Column(Boolean, nullable=False, server_default="false")
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
