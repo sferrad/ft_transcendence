d425b7fe54ed4b51101ed0cf80dfc5feb14b1acd
sferrad <ferradsabry@gmail.com>
2026-03-18 14:57:04 +0100

    Nouveau Background Home

--- srcs/frontend/src/log/useAuth.tsx
+++ srcs/frontend/src/log/useAuth.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { data, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 
