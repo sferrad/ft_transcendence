51306f1d138cca9f5b0909616c5cea4ac9256501
ankammer <ankammer@f1br6s19.paris.42.school>
2026-03-19 15:03:53 +0100

    FEAT: profile-service | CORR: refactorisation + correction

--- srcs/backend/user-service/app/main.py
+++ srcs/backend/user-service/app/main.py
@@ -1,4 +1,3 @@
-import os
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -6,6 +5,7 @@ from sqlalchemy.orm import Session
 from . import models
 from . import schemas, crud
 
+from .password import _hash_password, _verify_password
 from .database import get_db, init_db, init_engine
 
 
@@ -19,7 +19,7 @@ def on_startup() -> None:
 		init_engine()  # lit Vault -> crée engine + SessionLocal
 		init_db() # a mettre on event("startup")???
 	except Exception as e:
-    		print(f"[startup] DB init failed: {e}")
+		print(f"[startup] DB init failed: {e}")
 
 
 @app.get("/health")
@@ -36,9 +36,9 @@ async def db_ping(db: Session = Depends(get_db)):
 		raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@app.post("/auth/register")
+@app.post("/auth/register", response_model=schemas.OutputLogin)
 async def auth_register(payload: schemas.RegisterRequest, db: Session = Depends(get_db)):
-	hashed = crud.hash_password(payload.password)
+	hashed = _hash_password(payload.password)
 	user = models.User(email=payload.email, hashed_password=hashed, username=payload.username)
 	try:
 		user = crud.add_user(db, user)
@@ -48,13 +48,13 @@ async def auth_register(payload: schemas.RegisterRequest, db: Session = Depends(
 		if crud.existing_username(db, payload.username):
 			raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken")
 		raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
-	return {"id": user.id, "email": user.email, "username": user.username}
+	return schemas.OutputLogin(id=user.id, email=user.email, username=user.username)
 
 # Ajout
 @app.post("/internal/auth/verify")
 async def internal_auth_verify(payload: schemas.LoginRequest, db: Session = Depends(get_db)):
 	user = crud.get_user_by_identifier(db, payload.identifier)
-	if not user or not crud.verify_password(payload.password, user.hashed_password):
+	if not user or not _verify_password(payload.password, user.hashed_password):
 		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
-	return {"ok": True, "user": {"id": user.id, "email": user.email, "username": user.username}}
+	return {"ok": True, "user": schemas.OutputLogin(id=user.id, email=user.email, username=user.username)}
