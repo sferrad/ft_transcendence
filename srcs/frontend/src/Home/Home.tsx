986bab0353f77141030023e92a6c81e59fc6bdbf
sferrad <ferradsabry@gmail.com>
2026-03-23 06:35:50 +0100

    ajout du changement de langue dans les settings

--- srcs/frontend/src/Home/Home.tsx
+++ srcs/frontend/src/Home/Home.tsx
@@ -1,6 +1,5 @@
 import Carousel from './Carousel';
 import Profil from './Buttonprofile';
-import Trad from './Buttontrad';
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
@@ -8,13 +7,12 @@ const Home = () => {
     return (
         <div className="fixed inset-0 bg-[url('/assets/bgHome.png')] bg-cover bg-center bg-no-repeat">
             <div className="absolute top-0 left-0 right-0 flex justify-between items-center p-6 z-10">
-                <Trad />
                 {localStorage.getItem("access_token") ? (
                     <Profil />
                 ) : (
                     <div className="flex gap-4 items-center">
-                        <button onClick={() => window.location.href = "/login"} className='cursor-pointer px-6 py-2 text-lg text-white bg-blue-600 hover:bg-blue-700 rounded-lg transition-colors duration-200'>{t("Login")}</button>
-                        <button onClick={() => window.location.href = "/register"} className='cursor-pointer px-6 py-2 text-lg text-white bg-green-600 hover:bg-green-700 rounded-lg transition-colors duration-200'>{t("Register")}</button>
+                        <button onClick={() => window.location.href = "/login"} className='absolute right-6 cursor-pointer px-6 py-2 text-lg text-white bg-blue-600 hover:bg-blue-700 rounded-lg transition-colors duration-200'>{t("Login")}</button>
+                        <button onClick={() => window.location.href = "/register"} className='absolute right-45 cursor-pointer px-6 py-2 text-lg text-white bg-green-600 hover:bg-green-700 rounded-lg transition-colors duration-200'>{t("Register")}</button>
                     </div>
                 )}
             </div>
