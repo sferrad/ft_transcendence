f4a1797467f5050f5378eee309947a6b55130b09
Andy Kammerer <ankammer@f2r12s15.clusters.42paris.fr>
2026-05-14 14:30:51 +0200

    DELETE: suppression colonnes de tables inutilisees

--- srcs/backend/user-service/app/models.py
+++ srcs/backend/user-service/app/models.py
@@ -10,8 +10,5 @@ class User(Base):
     email = Column(String(50), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
 
-    is_active = Column(Boolean, nullable=False, server_default="true")
-    is_superuser = Column(Boolean, nullable=False, server_default="false")
-
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
